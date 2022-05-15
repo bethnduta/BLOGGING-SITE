b2a9a4becc029a7dc85595f4f60726bd0b6704ba
beth nduta <bethnduta05@gmail.com>
2022-05-15 05:07:16 +0300

    deployment

--- app/manage.py
+++ app/manage.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,6 +10,10 @@ def hello():
 @app.route('/')
 def blog():
     return render_template('blog.html')
+
+@app.route('/index')
+def index():
+    return render_template('macro.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
