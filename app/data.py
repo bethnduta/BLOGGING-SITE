9c21817ac54517899a8050be2aefce92fc08f54b
beth nduta <bethnduta05@gmail.com>
2022-05-16 00:41:31 +0300

    forms

---
+++ app/data.py
@@ -0,0 +1,21 @@
+# def Posts():
+#     posts= [
+#         {
+#             'id':1,
+#             'title':'my post',
+#             'body': 'this is my post',
+#             'author': 'beth'
+#         },
+#          {
+#             'id':2,
+#             'title':'Blog post',
+#             'body': 'this is my post',
+#             'author': 'nduta'
+#         },
+#           {
+#             'id':3,
+#             'title':'fancy posts',
+#             'body': 'this is my post',
+#             'author': 'kiarie'
+#         }
+#     ]
